a670358bade208a25ccff05dce9b37b68510ef94
jzsherlock <jzsherlock@163.com>
2020-09-24 15:12:18 +0800

    add unstable bandit

--- multi_armed_bandit/bandit.py
+++ multi_armed_bandit/bandit.py
@@ -12,6 +12,20 @@ class GaussianBandit():
         self.centers = np.random.permutation(num_arms)
         self.sigma = np.ones(num_arms) * sig
     def get_reward(self, arm_id=0):
+        #np.random.seed(int(time.time()))
+        reward = np.random.randn() * self.sigma[arm_id] + self.centers[arm_id]
+        return reward
+    
+class UnstableGaussianBandit(GaussianBandit):
+    def __init__(self, num_arms=5, sig=0.1, change_interval=100):
+        super(UnstableGaussianBandit, self).__init__(num_arms=num_arms, sig=sig)
+        self.change_interval = change_interval
+        self.counter = 0
+    def get_reward(self, arm_id=0):
+        self.counter += 1
+        if self.counter == self.change_interval:
+            self.centers = self.centers + np.random.randn(self.num_arms) * 3.0
+            self.counter = 0
         reward = np.random.randn() * self.sigma[arm_id] + self.centers[arm_id]
         return reward
 
---
+++ multi_armed_bandit/test_bandit.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+# filename: test_bandit.py
+# brief: test bandit class to simulate MAB
+# author: Jia Zhuang
+# date: 2020-09-24
+
+import numpy as np
+import matplotlib.pyplot as plt
+from bandit import GaussianBandit, UnstableGaussianBandit
+
+if __name__ == "__main__":
+
+    # test normal Gaussian Bandit
+    toy_bandit = GaussianBandit(num_arms=8)
+    print(toy_bandit.centers)
+    print(toy_bandit.get_reward(1))
+    print(toy_bandit.get_reward(2))
+
+    # test unstable Gaussian Bandit
+    toy_bandit = UnstableGaussianBandit(num_arms=3, sig=2.0, change_interval=100)
+    ls0, ls1, ls2 = [], [], []
+    cntr0, cntr1, cntr2 = [], [], []
+    for i in range(1000):
+        sa = np.random.choice(3)
+        r = toy_bandit.get_reward(sa)
+        cntr0.append(toy_bandit.centers[0])
+        cntr1.append(toy_bandit.centers[1])
+        cntr2.append(toy_bandit.centers[2])
+        if sa == 0:
+            ls0.append(r)
+        elif sa == 1:
+            ls1.append(r)
+        else:
+            ls2.append(r)
+    
+    # show the change of means of each arm reward 
+    # when bandit is unstable
+    fig = plt.figure(figsize=(10,10))
+    ax1 = fig.add_subplot(211)
+    ax2 = fig.add_subplot(212)
+    fig.subplots_adjust(wspace=None, hspace=0.3)
+    ax1.plot(ls0)
+    ax1.plot(ls1)
+    ax1.plot(ls2)
+    ax1.grid()
+    ax1.set_title('Rewards of Each Arm')
+    ax1.set_xlabel('Iterations')
+    ax1.set_ylabel('Reward')
+    ax2.plot(cntr0)
+    ax2.plot(cntr1)
+    ax2.plot(cntr2)
+    ax2.grid()
+    ax1.set_title('Mean of Each Arm of Gaussian Bandit (showing unstability)')
+    ax1.set_xlabel('Iterations')
+    ax1.set_ylabel('Current Mean of Reward')
+    plt.show()
